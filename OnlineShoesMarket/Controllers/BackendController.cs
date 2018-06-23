8e9489ef4e7cb54e3173f9a6f39203acacb9e8a0
wolfworker <1195747247@qq.com>
2018-06-23 15:28:32 +0800

    #修改样式及部分逻辑优化

--- OnlineShoesMarket/Controllers/BackendController.cs
+++ OnlineShoesMarket/Controllers/BackendController.cs
@@ -30,9 +30,9 @@ namespace OnlineShoesMarket.Controllers
 
                 if (request != null)
                 {
-                    if (!string.IsNullOrEmpty(request.OrderNo))
+                    if (!string.IsNullOrEmpty(request.Color))
                     {
-                        sql.Append($" AND ID = '{request.OrderNo}'");
+                        sql.Append($" AND Color LIKE '%{request.Color}%'");
                     }
                     if (!string.IsNullOrEmpty(request.ReceiveName))
                     {
@@ -40,17 +40,13 @@ namespace OnlineShoesMarket.Controllers
                     }
                     if (!string.IsNullOrEmpty(request.ReceivePhone))
                     {
-                        sql.Append($" AND ReceivePhone = '{request.ReceivePhone}'");
+                        sql.Append($" AND ReceivePhone LIKE '%{request.ReceivePhone}%'");
                     }
-                    if (!string.IsNullOrEmpty(request.ProductName))
+                    if (!string.IsNullOrEmpty(request.Size))
                     {
-                        sql.Append($" AND ProductName LIKE '%{request.ProductName}%'");
+                        sql.Append($" AND Size LIKE '%{request.Size}%'");
                     }
-                    if (!string.IsNullOrEmpty(request.Address))
-                    {
-                        sql.Append($" AND ReceiveAddress LIKE '%{request.Address}%'");
-                    }
-                    if (!string.IsNullOrEmpty(request.Status))
+                    if (!string.IsNullOrEmpty(request.Status) && request.Status!="0")
                     {
                         sql.Append($" AND Status = '{request.Status}'");
                     }
@@ -70,17 +66,38 @@ namespace OnlineShoesMarket.Controllers
                 {
                     result.Add(new OrderDetail
                     {
-                        ID = item["ID"].ToString(),
+                        //ID = item["ID"].ToString(),
+                        //ProductID = item["ProductID"].ToString(),
+                        //ProductName = item["ProductName"].ToString(),
+                        //UnitPrice = decimal.Parse(item["UnitPrice"].ToString()),
+                        //ReceivePhone = item["ReceivePhone"].ToString(),
+                        //ReceiveAddress = item["ReceiveAddress"].ToString(),
+                        //ReceiveName = item["ReceiveName"].ToString(),
+                        //CreateTime = DateTime.Parse(item["CreateTime"].ToString()),
+                        //Count = int.Parse(item["Count"].ToString()),
+                        //Status = short.Parse(item["Status"].ToString()),
+                        //TotalPrice = string.IsNullOrEmpty(item["TotalPrice"].ToString()) ? decimal.Parse("0") : decimal.Parse(item["TotalPrice"].ToString())
+
+                         ID = item["ID"].ToString(),
                         ProductID = item["ProductID"].ToString(),
                         ProductName = item["ProductName"].ToString(),
                         UnitPrice = decimal.Parse(item["UnitPrice"].ToString()),
                         ReceivePhone = item["ReceivePhone"].ToString(),
                         ReceiveAddress = item["ReceiveAddress"].ToString(),
                         ReceiveName = item["ReceiveName"].ToString(),
+                        Color = item["Color"].ToString(),
+                        Size = item["Size"].ToString(),
+                        PayType = short.Parse(item["PayType"].ToString()),
+                        Remarks = item["Remarks"].ToString(),
+                        ReceiveCityID = item["ReceiveCityID"].ToString(),
+                        CreateUser = item["CreateUser"].ToString(),
+                        UpdateUser = item["UpdateUser"].ToString(),
+                        UpdateTime = DateTime.Parse(item["UpdateTime"].ToString()),
                         CreateTime = DateTime.Parse(item["CreateTime"].ToString()),
                         Count = int.Parse(item["Count"].ToString()),
                         Status = short.Parse(item["Status"].ToString()),
                         TotalPrice = string.IsNullOrEmpty(item["TotalPrice"].ToString()) ? decimal.Parse("0") : decimal.Parse(item["TotalPrice"].ToString())
+
                     });
                 }
             }
@@ -194,11 +211,10 @@ namespace OnlineShoesMarket.Controllers
 
     public class OrderRequest
     {
-        public string OrderNo { get; set; }
+        public string Color { get; set; }
         public string ReceivePhone { get; set; }
         public string ReceiveName { get; set; }
-        public string ProductName { get; set; }
         public string Status { get; set; }
-        public string Address { get; set; }
+        public string Size { get; set; }
     }
 }

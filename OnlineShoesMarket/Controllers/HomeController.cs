203e66fe2b078e10fb0d3146a1d8a66d784845e3
wolfworker <1195747247@qq.com>
2019-04-18 20:47:59 +0800

    1：增加站点主页

---
+++ OnlineShoesMarket/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoesMarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
